6c9f3a2553eb48547817b388495f8829b490de06
Lammax <lammax@mail.ru>
2020-02-25 11:26:55 +0300

    Initial Commit

---
+++ SoundVisualizer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SoundVisualizer
+//
+//  Created by Mac on 25.02.2020.
+//  Copyright © 2020 Lammax. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
